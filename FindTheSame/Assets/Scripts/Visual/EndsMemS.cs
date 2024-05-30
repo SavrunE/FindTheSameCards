c305e9c0683b4245b620ece3c5d66d0b54756eaa
SavrunE <69328877+SavrunE@users.noreply.github.com>
2024-05-31 00:01:39 +0300

    add win lose menu

---
+++ FindTheSame/Assets/Scripts/Visual/EndsMemS.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndsMemS : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _winSprites;
+    [SerializeField] private Sprite[] _loseSprites;
+}
