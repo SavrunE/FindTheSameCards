d078efc68ef79ec997df5b20fb1fb5c9ec2cd6c9
SavrunE <69328877+SavrunE@users.noreply.github.com>
2024-06-05 17:21:42 +0300

    start create win lose sprites

--- FindTheSame/Assets/Scripts/Visual/EndsMemS.cs
+++ FindTheSame/Assets/Scripts/Visual/EndsMemS.cs
@@ -1,9 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EndsMemS : MonoBehaviour
 {
     [SerializeField] private Sprite[] _winSprites;
     [SerializeField] private Sprite[] _loseSprites;
+
+    public Sprite TakeWinSprite(int index)
+    {
+        return _winSprites[index];
+    }
+
+    public Sprite TakeLoseSprite(int index)
+    {
+        return _loseSprites[index];
+	}
+
+    public int TakeWinLenth()
+    {
+        return _winSprites.Length;
+    }
+
+    public int TakeLostLenth()
+    {
+		return _loseSprites.Length;
+    }
 }
--- FindTheSame/Assets/Scripts/Visual/WinLoseMenu.cs
+++ FindTheSame/Assets/Scripts/Visual/WinLoseMenu.cs
@@ -1,18 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinLoseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private EndsMemS _endsMemS;
+	[SerializeField] private List<Image> _images;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		PlaceWinSprites();
+
+	}
+
+	public void PlaceWinSprites()
+	{
+		List<int> numbers = FindNumbers(_images.Count, _endsMemS.TakeWinLenth());
+
+		for (int i = 0; i < numbers.Count; i++)
+		{
+			_images[i].sprite = _endsMemS.TakeWinSprite(numbers[i]);
+		}
+	}
+
+	private List<int> FindNumbers(int returnCount, int generalCount)
+	{
+		List<int> numbers = new List<int>();
+		List<int> result = new List<int>();
+
+		for (int i = 0; i < generalCount; i++)
+		{
+			numbers.Add(i);
+		}
+
+		for (int x = 0; x < returnCount; x++)
+		{
+			int rnd = Random.Range(0, generalCount);
+			numbers.Remove(rnd);
+			result.Add(rnd);
+		}
+
+		return result;
+	}
 }
