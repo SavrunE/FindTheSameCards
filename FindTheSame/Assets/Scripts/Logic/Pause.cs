cc1547e0189668ac4cd05af5c1f683610b0209a9
SavrunE <69328877+SavrunE@users.noreply.github.com>
2024-06-23 08:49:09 +0300

    pause

--- FindTheSame/Assets/Scripts/Logic/Pause.cs
+++ FindTheSame/Assets/Scripts/Logic/Pause.cs
@@ -1,16 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] private PauseMenuView _pauseMenu;
+    private bool _isPaused = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Debug.Log("Stop");
-
+            if (_isPaused == false)
+            {
+                _isPaused = true;
+				_pauseMenu.gameObject.SetActive(true);
+				_pauseMenu.OpenMenu();
+				Debug.Log("Pause");
+			}
+            else
+			{
+                Continue();
+			}
         }
-        
+        if (_isPaused && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+			Continue();
+		}
     }
+
+    private void Continue()
+    {
+		_isPaused = false;
+		_pauseMenu.CloseMenu();
+		Debug.Log("Play");
+		_pauseMenu.gameObject.SetActive(false);
+	}
 }
--- FindTheSame/Assets/Scripts/Visual/PauseMenuView.cs
+++ FindTheSame/Assets/Scripts/Visual/PauseMenuView.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +6,30 @@ public class PauseMenuView : MonoBehaviour
     [SerializeField] private Image[] _images;
     [SerializeField] private EndsMemS _mems;
 
-    private void Start()
-    {
-        OpenMenu();
-
-	}
-
     public void OpenMenu()
     {
-        var s = _mems.TakeRndSprites(_images.Length);
+		UsePause(true);
+		var s = _mems.TakeRndSprites(_images.Length);
         for (int i = 0; i < _images.Length; i++)
         {
             _images[i].sprite = s[i];
-
 		}
 	}
 
     public void CloseMenu()
     {
+		UsePause(false);
+	}
 
-    }
+	private void UsePause(bool value)
+	{
+		if (value)
+		{
+			Time.timeScale = 0f;
+		}
+		else
+		{
+			Time.timeScale = 1f;
+		}
+	}
 }
